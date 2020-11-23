24f9515fb68536f1761a24a514c537ae8bc4e48d
Kuerl <ititiu19047@student.hcmiu.edu.vn>
2020-11-24 03:06:46 +0700

    add libgdx

---
+++ JavaApp/PlayScreen/core/src/com/kuerlgame/drawSprite.java
@@ -0,0 +1,12 @@
+package com.kuerlgame;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class drawSprite {
+    private TextureAtlas drawAtlas;
+    private
+
+    public void drawSprite() {
+
+    }
+}
