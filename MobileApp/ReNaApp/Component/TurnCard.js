4bc2749a23f935dc312726111d7194771ac90de5
Datdeptrai0609 <Datdeptrai0609>
2020-12-19 00:32:52 +0700

    add turncard img and blank file js

---
+++ MobileApp/ReNaApp/Component/TurnCard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    ImageBackground,
+    Image,
+    Dimensions,
+    BackHandler,
+    Alert
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default class TurnCard extends Component {
+    render() {
+        return (
+           <View>
+               
+           </View>
+        )
+    }
+}
