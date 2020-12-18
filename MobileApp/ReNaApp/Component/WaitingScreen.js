b9ac98e3bee391418d52df3c42f59cb5387d9439
Datdeptrai0609 <Datdeptrai0609>
2020-12-18 23:11:51 +0700

    add comment

--- MobileApp/ReNaApp/Component/WaitingScreen.js
+++ MobileApp/ReNaApp/Component/WaitingScreen.js
@@ -26,26 +26,26 @@ import { Actions } from 'react-native-router-flux';
 export default class Waiting extends Component {
     state= {
         show: false,// status show or hide waiting GIF
-        move:false,
+        move:false,//status for rout to next screen
     }
 
     UNSAFE_componentWillMount() {
         this.interval = setInterval(() => {
-            this.setState({show : true})
+            this.setState({show : true})//show GIF after 1.5s
         },1500);
         this.intervalMove = setInterval(() => {
-            this.setState({move: true})
+            this.setState({move: true}) //set state move to true after 1.5s
             if (this.state.move == true) {
-                Actions.gameScreen();
+                Actions.gameScreen(); // move to gameScreen if status of move is true
             }
         },3500);
     }
 
     componentDidUpdate() {
-        if (this.state.show == true) {
+        if (this.state.show == true) {// if set state for show done, clear it
             clearInterval(this.interval)
         }
-        if(this.state.move == true) {
+        if(this.state.move == true) {//if moved to next screen, clear it
             clearInterval(this.intervalMove)
         }
         
