dc4820fd78a89fe1542019a50ad0aada8aa59943
Datdeptrai0609 <Datdeptrai0609>
2020-12-18 23:28:25 +0700

    create default GameScreen and fix router

---
+++ MobileApp/ReNaApp/Component/GameScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    ImageBackground,
+    Image,
+    Dimensions,
+    BackHandler,
+    Alert
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
