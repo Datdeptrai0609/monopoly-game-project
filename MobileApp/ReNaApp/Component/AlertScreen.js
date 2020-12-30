3a6e818a77d0c755a05f6759257cf7f47be313d7
Datdeptrai0609 <Datdeptrai0609>
2020-12-30 13:28:40 +0700

    create black AlertScreen

---
+++ MobileApp/ReNaApp/Component/AlertScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    ImageBackground,
+    Image,
+    Dimensions,
+    BackHandler,
+    Alert
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
