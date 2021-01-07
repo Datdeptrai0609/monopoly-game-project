073c973d4256e580bce0207dc882509efd9886db
Anh Tai <nducanhtai@gmail.com>
2021-01-08 06:32:42 +0700

    Add plane

--- JavaApp/PlayScreen/core/src/com/monopoly/game/gameGUI/Aircraft.java
+++ JavaApp/PlayScreen/core/src/com/monopoly/game/gameGUI/Aircraft.java
@@ -6,14 +6,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class Aircraft {
-  private Texture aircraftImg;
+  private Texture rocketImg, planeImg;
   private Texture effect;
   private SpriteBatch sb;
   // private long launchTime, landTime;
-  private long time;
-  private boolean isLaunching, isLanding;
-  private long duration;
+  private long rocketTime, planeTime;
+  private boolean isLaunching, isLanding, planeStart;
+  private long rocketDuration, planeDuration;
   private float x, y;
+  private float xPlane, yPlane;
 
   private enum Status {
     LAUNCH, LAND
@@ -22,43 +23,56 @@ public class Aircraft {
   private Status aircraftStatus;
 
   public Aircraft(SpriteBatch sb) {
-    aircraftImg = new Texture("aircraft/rocket.png");
+    rocketImg = new Texture("aircraft/rocket.png");
+    planeImg = new Texture("playScreenAssets/PlayerTwo/f52.png");
     effect = new Texture("aircraft/effect.png");
     this.sb = sb;
-    duration = Long.valueOf(10) * 1000000000;
+    rocketDuration = Long.valueOf(10) * 1000000000;
+    planeDuration = Long.valueOf(15) * 1000000000;
     x = Gdx.graphics.getWidth() * 0.4f;
     y = Gdx.graphics.getHeight() * 0.4f;
-    time = TimeUtils.nanoTime();
-    isLaunching = isLanding = false;
+    xPlane = yPlane = 0;
+    rocketTime = planeTime = TimeUtils.nanoTime();
+    isLaunching = isLanding = planeStart = false;
     aircraftStatus = Status.LAUNCH;
   }
 
   public void render() {
-    sb.draw(aircraftImg, x, y, aircraftImg.getWidth() * 0.8f, aircraftImg.getHeight() * 0.8f);
+    sb.draw(rocketImg, x, y, rocketImg.getWidth() * 0.8f, rocketImg.getHeight() * 0.8f);
     // Handle render
     if (isLaunching) {
-      sb.draw(effect, x + (aircraftImg.getWidth() * 0.8f - effect.getWidth() * 2) / 2, y - effect.getHeight() * 2 + 10,
+      sb.draw(effect, x + (rocketImg.getWidth() * 0.8f - effect.getWidth() * 2) / 2, y - effect.getHeight() * 2 + 10,
           effect.getWidth() * 2, effect.getHeight() * 2);
       y += 250 * Gdx.graphics.getDeltaTime();
       if (y > Gdx.graphics.getHeight()) {
         isLaunching = false;
-        time = TimeUtils.nanoTime();
+        rocketTime = TimeUtils.nanoTime();
         aircraftStatus = Status.LAND;
       }
-    }
-    if (isLanding) {
-      sb.draw(effect, x + (aircraftImg.getWidth() * 0.8f - effect.getWidth() * 2) / 2, y - effect.getHeight() * 2 + 10,
+    } else if (isLanding) {
+      sb.draw(effect, x + (rocketImg.getWidth() * 0.8f - effect.getWidth() * 2) / 2, y - effect.getHeight() * 2 + 10,
           effect.getWidth() * 2, effect.getHeight() * 2);
       y -= 200 * Gdx.graphics.getDeltaTime();
       if (y < Gdx.graphics.getHeight() * 0.4f) {
         isLanding = false;
-        time = TimeUtils.nanoTime();
+        rocketTime = TimeUtils.nanoTime();
         aircraftStatus = Status.LAUNCH;
       }
     }
 
-    // Check time
-    if (TimeUtils.nanoTime() - time > duration) {
+    if (planeStart) {
+      sb.draw(planeImg, xPlane, yPlane);
+      xPlane += 600 * Gdx.graphics.getDeltaTime();
+      yPlane = (float) (0.55555 * xPlane);
+      if (xPlane > Gdx.graphics.getWidth()) {
+        planeStart = false;
+        planeTime = TimeUtils.nanoTime();
+        xPlane = yPlane = 0;
+      }
+    }
+
+    // Check rocketTime
+    if (TimeUtils.nanoTime() - rocketTime > rocketDuration) {
       switch (aircraftStatus) {
         case LAUNCH:
           isLaunching = true;
@@ -67,6 +81,9 @@ public class Aircraft {
           isLanding = true;
           break;
       }
+    }
+    if (TimeUtils.nanoTime() - planeTime > planeDuration) {
+      planeStart = true;
     }
   }
 }
